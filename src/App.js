1617b53d81547439405b1977c254441e59e33226
clstyn <fiorenzacelestyn@gmail.com>
2023-03-15 20:24:17 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex justify-center items-center h-screen">
+      <h1 className="text-5xl">
+        DestiMatch
+      </h1>
+    </div>
+  );
+}
+
+export default App;
