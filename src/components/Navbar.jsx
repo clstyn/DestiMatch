3bb8b569462faa9c389df61149220ce36df13c02
clstyn <fiorenzacelestyn@gmail.com>
2023-03-22 20:56:09 +0700

    fix warnings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUser } from "@fortawesome/free-solid-svg-icons";
 import UserIcon from "../assets/user1.svg"
 
 export const Navbar = () => {
