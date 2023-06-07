55179ba0ffacf03e84fa0e5f49b6839f0fd539f9
pramudyadika <pramudyakusuma.pk@gmail.com>
2023-06-07 15:57:07 +0700

    Fix Home Layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,25 @@ import React from "react";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faUser } from "@fortawesome/free-solid-svg-icons";
 import UserIcon from "../assets/user1.svg"
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center justify-between py-7 font-poppins px-32 py-8 absolute top-0 w-full">
-      <button className="font-bold text-[28px]">
+    <div className="flex items-center justify-between font-poppins px-32 pt-4 absolute top-0 w-full">
+      <Link className="font-bold text-2xl" to='/'>
         <span className="text-textblack">Desti</span>
         <span className="text-pink1">Match</span>
-      </button>
+      </Link>
 
       <div className="flex gap-[52px]">
-        <button className="font-semibold text-[24px]">Beranda</button>
-        <button className="font-semibold text-[24px]">Riwayat</button>
-        <button className="font-semibold text-[24px]">Tentang</button>
+        <Link className="font-semibold text-lg" to ='/'>Beranda</Link>
+        <button className="font-semibold text-lg">Riwayat</button>
+        <Link className="font-semibold text-lg" to='/'>Tentang</Link>
       </div>
 
-      <button className="h-[50px] w-[50px] p-1 mr-2">
+      <Link className="h-8 w-8 pt mr-2" to='/profile'>
         <img src={UserIcon} alt="profile" />
-      </button>
+      </Link>
     </div>
   );
 };
--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -10,26 +10,26 @@ export const Home1 = () => {
       <Navbar />
       <div className="h-screen flex items-center justify-center bg-white2 ">
         <div className="flex max-w-[1200px]">
-            <div className="w-[544px] mr-[132px] flex flex-col">
-                <p className="text-[108px] font-semibold font-poppins">Explore Beautiful Indonesia</p>
-                <div className="rounded-[20px] bg-white shadow-home flex justify-between">
-                    <div className="px-4 py-8 h-full ml-4">
-                        <label className="font-poppins text-2xl">Lokasi Kamu</label>
-                        <select name="city" id="city" className="text-2xl font-poppins text-pink1 font-bold">
-                            <option value="bandung">Bandung</option>
+            <div className="w-[544px] mr-[132px] flex flex-col justify-center gap-8">
+                <p className="text-8xl font-semibold font-poppins">Explore Beautiful Indonesia</p>
+                <div className="rounded-xl bg-white shadow-home flex justify-between h-16">
+                    <div className="px-4 py-4 h-full ml-4">
+                        {/* <label className="font-poppins text-sm w-full">Tipe Wisata Pilihanmu</label> */}
+                        <select name="city" id="city" className="text-lg font-poppins text-pink1 font-medium">
+                            <option value="Lorem Ipsum" className="text-lg">Lorem Ipsum</option>
+                            <option value="Lorem" className="text-lg">Lorem</option>
+                            <option value="Ipsum" className="text-lg">Ipsum</option>
                         </select>
                     </div>
                     <div className="py-2">                        
                         <hr vertical="true" className="bg-pink1 w-[4px] h-full"/>
                     </div>
-                    <div className="px-4 py-8 h-full">
-                        <label className="font-poppins text-2xl">Tipe Wisata</label>
-                        <select name="city" id="city" className="text-2xl font-poppins text-pink1 font-bold">
-                            <option value="alam">Alam</option>
-                        </select>
+                    <div className="px-2 py-4 h-full">
+                        {/* <label className="font-poppins text-2xl">Nama Wisata</label> */}
+                        <input name="city" id="city" className="text-lg px-3 font-poppins font-medium items-center" placeholder="Nama Wisata"/>
                     </div>
-                    <button className="bg-pink1 flex items-center justify-center p-8 rounded-r-[20px]">
-                        <img src={SearchIcon} alt="search" />
+                    <button className="bg-pink1 flex items-center justify-center p-3 rounded-r-xl">
+                        <img src={SearchIcon} alt="search"  />
                     </button>
                 </div>
             </div>
